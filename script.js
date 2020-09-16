a93ac33667dae6b791e944bc541b5a8949d58f78
John R. Estes <John.R.Estes@icloud.com>
2020-09-15 21:35:30 -0400

    added html/css/js

---
+++ script.js
@@ -0,0 +1,84 @@
+$(document).ready(function() {
+
+    var currentData = null;
+    var th = "";
+
+    $(function (){
+
+        $.ajax({
+            url: 'http://localhost:3000/api/movies',
+            dataType: "json",
+            type: 'GET',
+            success: function(data){
+                currentData = data;
+                console.log(data);
+                buildTable(currentData);
+            }
+        });
+    });
+
+    function buildTable(data){
+        var $jsonData = $('#jsonData');
+        var songTemplate = "" +
+        "<tr>" +
+        "<td>{{title}}</td>" +
+        "<td>{{album}}</td>" +
+        "<td>{{artist}}</td>" +
+        "<td>{{genre}}</td>" +
+        "<td>{{releaseDate}}</td>" + 
+        "</tr>";
+
+        $.each(data, function(i, jsonData) {
+            $jsonData.append(Mustache.render(songTemplate, jsonData));
+        });
+    };
+
+    function filterResults(currentData, th, str){
+        var newResults = $(currentData).filter(function(i) {
+            return currentData[i][th] === str;
+        });
+        $("#jsonData > tr").remove();
+        buildTable(newResults);
+    };
+
+    function inputSearch(th){
+        $("#" + th).empty();
+        $("#" + th + "Input").fadeIn();
+        $("#btn").fadeIn();
+        $("#reset").fadeIn();
+        $("#btn").click(function(){
+            var str = $("#" + th + "Input").val();
+            filterResults(currentData, th, str);
+        });
+    };
+
+    $("#reset").click(function(){
+        window.location.reload();
+    });
+
+    $("#title").click(function(event) {
+        th = "title";
+        inputSearch(th);
+    });
+
+    $("#album").click(function(event) {
+        th = "album";
+        inputSearch(th);
+    });
+
+    $("#artist").click(function(event) {
+        th = "artist";
+        inputSearch(th);
+    });
+
+    $("#genre").click(function(event) {
+        th = "genre";
+        inputSearch(th);
+    });
+
+    $("#releaseDate").click(function(event) {
+        th = "releaseDate";
+        inputSearch(th);
+    });
+
+});
