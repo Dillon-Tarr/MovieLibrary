72c3e16a467acf7a4fa04255eb90eb487447febe
Dillon Tarr <56458494+Dillon-Tarr@users.noreply.github.com>
2020-09-15 20:09:12 -0400

    added post request for adding a new movie

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(3000, function (){
   console.log("Server started. Listening on port 3000.");
 });
@@ -17,4 +20,10 @@ app.get("/api/movies/:id", (req, res) => {
   let id = req.params.id;
   let movie = repoContext.movies.findMovieById(id);
   res.send(movie);
+});
+
+app.post("/api/movies", (req, res) => {
+  let newMovie = req.body;
+  let addedMovie = repoContext.movies.createMovie(newMovie);
+  res.send(addedMovie);
 });
